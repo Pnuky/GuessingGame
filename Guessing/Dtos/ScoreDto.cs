1b575ef9ce87dbc1a233ae7ad7c15f1d22e29856
PNUKPC\Mac <maciejkitaiphone@gmail.com>
2022-08-30 22:24:50 +0200

    dtos&models

---
+++ Guessing/Dtos/ScoreDto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Guessing.Dtos
+{
+    public class ScoreDto
+    {   
+        public Guid SessionId { get; set; }
+        public int GuessingTimes { get; set; }
+        public double PlayTime { get; set; }
+
+    }
+}
---
+++ Guessing/Models/Score.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Guessing.Models
+{
+    public class Score
+    {
+        public Score(Guid sessionId)
+        { 
+            SessionId = sessionId;
+            GameStart = DateTime.Now;
+        }
+
+        public Guid SessionId { get; set; }
+        public int GuessingTimes { get; set; }
+        public DateTime GameStart { get; set; } 
+
+        public double GameTimeCalc()
+        {
+            var calctime = (DateTime.Now - GameStart).TotalSeconds;
+
+            return calctime;
+        }
+
+    }
+}
