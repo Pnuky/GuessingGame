7a85dbf1244e5312651f971e5cfc5f9df1933cb1
PNUKPC\Mac <maciejkitaiphone@gmail.com>
2022-08-30 22:50:29 +0200

    Dodanie kontrolera

---
+++ Guessing/Controllers/NumberGuessingController.cs
@@ -0,0 +1,65 @@
+﻿using Guessing.Dtos;
+using Guessing.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Guessing.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class NumberGuessingController : ControllerBase
+    {
+
+        private static readonly List<Score> _games = new();
+        public static int _number;
+
+        public NumberGuessingController()
+        {
+          
+        }
+
+        [HttpGet,Route("start")]
+        public Guid Start()
+        {
+            var sessionId = Guid.NewGuid();
+            _number = new Random().Next(1,100);
+            _games.Add(new Score(sessionId));
+            return sessionId;
+        }
+
+        [HttpPost, Route("guess")]
+        public ActionResult<GuessResponseDto> Guess([FromBody] GuessDto guess)
+        {
+            var game = _games.SingleOrDefault(a => a.SessionId == guess.SessionId);
+
+            game.GuessingTimes++;
+
+            var result = guess.Number > _number ? "za duża" : guess.Number < _number ? "za mała" : "trafiona";
+
+            return Ok(new GuessResponseDto
+            {
+                GuessingTimes = game.GuessingTimes,
+                Result = result,
+                SessionId = guess.SessionId
+
+            });
+        }
+
+        public IEnumerable<ScoreDto> HighScore()
+        {
+            return _games.OrderByDescending(a => a.GuessingTimes)
+                .Take(10)
+                .Select(a => new ScoreDto
+                {
+                    GuessingTimes = a.GuessingTimes,
+                    SessionId = a.SessionId,
+                    PlayTime = a.GameTimeCalc()
+
+                });
+        }
+    }
+}
